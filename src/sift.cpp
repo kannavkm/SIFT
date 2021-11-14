05c5767ec7c24639bb42fe00572044c145577b06
Kannav Mehta <kmkannavkmehta@gmail.com>
2021-11-15 01:11:41 +0530

    [wip]opencv try

---
+++ src/sift.cpp
@@ -0,0 +1,5 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/ml/ml.hpp>
+
---
+++ src/sift.hpp
@@ -0,0 +1,21 @@
+#ifndef SIFT_H
+#define SIFT_H
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/ml/ml.hpp>
+
+namespace sift {
+
+class sift_handler {
+    int32_t octaves;
+    int32_t scales;          
+
+    vector<cv::Mat> img;
+
+};
+
+}
+
+#endif

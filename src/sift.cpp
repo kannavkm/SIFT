c09b9287d31419bcb05dfbab5f49d49ffdb60d63
Triansh <trianshsharma@gmail.com>
2021-11-20 19:50:43 +0530

    added photos

--- src/sift.cpp
+++ src/sift.cpp
@@ -67,14 +67,17 @@ void sift_handler::exec() {
     cv::Mat out, temp;
     onex.convertTo(temp, CV_8U);
     cv::drawKeypoints(temp, keypoints, out, cv::Scalar_<double>::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
-    cv::imshow("Display Image", out);
+    cv::imwrite("Display-Image-house.png", out);
     cv::waitKey(0);
-    // for (auto &octave : images) {
-    //     for (auto &img : octave) {
-    //         cv::imshow("Display Image", img);
-    //         cv::waitKey(0);
-    //     }
-    // }
+
+     for (int oct = 0; oct < images.size(); oct++) {
+         for (int img = 0; img < images[oct].size(); img++) {
+             images[oct][img] *= 255;
+             images[oct][img].convertTo(temp, CV_8U);
+             std::string name = "house-" + std::to_string(oct) + '-' + std::to_string(img) +  ".png";
+             cv::imwrite(name, temp);
+         }
+     }
 }
 
 /**
